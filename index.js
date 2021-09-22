5d906f04ed6d64dc26a68ee326ab6daf70f0c54a
Alaricode <antonlarichev@gmail.com>
2021-09-22 12:46:04 +0300

    Маршрутизация

--- index.js
+++ index.js
@@ -3,9 +3,25 @@ import express from 'express';
 const port = 8000;
 const app = express();
 
-app.get('/hello', (req, res) => {
-	res.send('Привет!');
+app.all('/hello', (req, res, next) => {
+	console.log('All');
+	next();
 });
+
+const cb = (req, res, next) => {
+	console.log('CB');
+	next();
+}
+
+app.route('/user')
+	.get('/hello', (req, res) => {
+		res.send('Привет!');
+	})
+	.post('/hello', (req, res) => {
+		res.send('Привет!');
+	});
+
+
 
 app.listen(port, () => {
 	console.log(`Сервер запущен на http://localhost:${port}`);
