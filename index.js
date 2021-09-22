093f2fec6ebd71e27aa790e7504152e00cfad640
Alaricode <antonlarichev@gmail.com>
2021-09-22 16:30:25 +0300

    Ответы клиенту

--- index.js
+++ index.js
@@ -3,25 +3,10 @@ import express from 'express';
 const port = 8000;
 const app = express();
 
-app.all('/hello', (req, res, next) => {
-	console.log('All');
-	next();
+app.get('/hello', (req, res) => {
+	// res.send('Привет!');
+	res.end();
 });
-
-const cb = (req, res, next) => {
-	console.log('CB');
-	next();
-}
-
-app.route('/user')
-	.get('/hello', (req, res) => {
-		res.send('Привет!');
-	})
-	.post('/hello', (req, res) => {
-		res.send('Привет!');
-	});
-
-
 
 app.listen(port, () => {
 	console.log(`Сервер запущен на http://localhost:${port}`);
