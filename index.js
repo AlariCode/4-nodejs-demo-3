8a58e2f07be61880061418a94b417269ed078de2
Alaricode <antonlarichev@gmail.com>
2021-09-24 18:01:25 +0300

    Промежуточные обработчики

--- index.js
+++ index.js
@@ -4,12 +4,21 @@ import { userRouter } from './users/users.js';
 const port = 8000;
 const app = express();
 
+app.use((req, res, next) => {
+	console.log('Время ', Date.now());
+	next();
+});
+
 app.get('/hello', (req, res) => {
-	// res.send('Привет!');
-	res.end();
+	throw new Error('Error!!!');
 });
 
 app.use('/users', userRouter);
+
+app.use((err, req, res, next) => {
+	console.log(err.message);
+	res.status(401).send(err.message);
+});
 
 app.listen(port, () => {
 	console.log(`Сервер запущен на http://localhost:${port}`);
--- users/users.js
+++ users/users.js
@@ -2,6 +2,11 @@ import express from 'express';
 
 const userRouter = express.Router();
 
+userRouter.use((req, res, next) => {
+	console.log('Обработчик users');
+	next();
+});
+
 userRouter.post('/login', (req, res) => {
 	res.send('login');
 });
