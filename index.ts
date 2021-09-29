53256d101c229eae5b58aa03964f83fd7f72c701
Alaricode <antonlarichev@gmail.com>
2021-09-29 10:17:01 +0300

    Начало работы с TypeScript

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import { userRouter } from './users/users.js';
 
 const port = 8000;
@@ -15,7 +15,7 @@ app.get('/hello', (req, res) => {
 
 app.use('/users', userRouter);
 
-app.use((err, req, res, next) => {
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
 	console.log(err.message);
 	res.status(401).send(err.message);
 });
