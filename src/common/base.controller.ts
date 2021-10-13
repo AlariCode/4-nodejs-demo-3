106154f161a26c944fdb9ba0bf665eda42179fe6
Alaricode <antonlarichev@gmail.com>
2021-10-13 13:08:32 +0300

    Базовый класс контроллера

---
+++ src/common/base.controller.ts
@@ -0,0 +1,37 @@
+import { Response, Router } from 'express';
+import { LoggerService } from '../logger/logger.service';
+import { IControllerRoute } from './route.interface';
+export { Router } from 'express';
+
+export abstract class BaseController {
+	private readonly _router: Router;
+
+	constructor(private logger: LoggerService) {
+		this._router = Router();
+	}
+
+	get router() {
+		return this._router;
+	}
+
+	public send<T>(res: Response, code: number, message: T) {
+		res.type('application/json')
+		return res.status(code).json(message);
+	}
+
+	public ok<T>(res: Response, message: T) {
+		return this.send<T>(res, 200, message);
+	}
+
+	public created(res: Response) {
+		return res.sendStatus(201);
+	}
+
+	protected bindRoutes(routes: IControllerRoute[]) {
+		for (const route of routes) {
+			this.logger.log(`[${route.method}] ${route.path}`);
+			const handler = route.func.bind(this);
+			this.router[route.method](route.path, handler);
+		}
+	}
+}
---
+++ src/common/route.interface.ts
@@ -0,0 +1,7 @@
+import { NextFunction, Request, Response, Router } from 'express';
+
+export interface IControllerRoute {
+	path: string;
+	func: (req: Request, res: Response, next: NextFunction) => void;
+	method: keyof Pick<Router, 'get' | 'post' | 'delete' | 'patch' | 'put'>;
+}
