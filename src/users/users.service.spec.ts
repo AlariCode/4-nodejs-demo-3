f8c59bd03f75c51366cfe7669e69281b04af0bc4
Alaricode <antonlarichev@gmail.com>
2021-10-23 10:21:56 +0300

    Упражнение - Новые unit тесты

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -53,4 +53,31 @@ describe('User Service', () => {
 		expect(createdUser?.id).toEqual(1);
 		expect(createdUser?.password).not.toEqual('1');
 	});
+
+	it('validateUser - success', async () => {
+		usersRepository.find = jest.fn().mockReturnValueOnce(createdUser);
+		const res = await usersService.validateUser({
+			email: 'a@a.ru',
+			password: '1',
+		});
+		expect(res).toBeTruthy();
+	});
+
+	it('validateUser - wrong password', async () => {
+		usersRepository.find = jest.fn().mockReturnValueOnce(createdUser);
+		const res = await usersService.validateUser({
+			email: 'a@a.ru',
+			password: '2',
+		});
+		expect(res).toBeFalsy();
+	});
+
+	it('validateUser - wrong user', async () => {
+		usersRepository.find = jest.fn().mockReturnValueOnce(null);
+		const res = await usersService.validateUser({
+			email: 'a2@a.ru',
+			password: '2',
+		});
+		expect(res).toBeFalsy();
+	});
 });
