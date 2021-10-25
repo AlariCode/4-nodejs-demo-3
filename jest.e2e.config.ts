fa83948a4f03fcb388eee43444623ee47c4b9bdf
Alaricode <antonlarichev@gmail.com>
2021-10-25 18:48:02 +0300

    Упражнение - Дописываем e2e тесты

--- jest.e2e.config.ts
+++ jest.e2e.config.ts
@@ -3,7 +3,6 @@ import type { Config } from '@jest/types';
 const config: Config.InitialOptions = {
 	verbose: true,
 	preset: 'ts-jest',
-	rootDir: './tests',
 	testRegex: '.e2e-spec.ts$',
 };
 
--- tests/users.e2e-spec.ts
+++ tests/users.e2e-spec.ts
@@ -16,6 +16,35 @@ describe('Users e2e', () => {
 			.send({ email: 'a@a.ru', password: '1' });
 		expect(res.statusCode).toBe(422);
 	});
+
+	it('Login - success', async () => {
+		const res = await request(application.app)
+			.post('/users/login')
+			.send({ email: 'a@a.ru', password: 'asdasdasd' });
+		expect(res.body.jwt).not.toBeUndefined();
+	});
+
+	it('Login - error', async () => {
+		const res = await request(application.app)
+			.post('/users/login')
+			.send({ email: 'a@a.ru', password: '1' });
+		expect(res.statusCode).toBe(401);
+	});
+
+	it('Info - success', async () => {
+		const login = await request(application.app)
+			.post('/users/login')
+			.send({ email: 'a@a.ru', password: 'asdasdasd' });
+		const res = await request(application.app)
+			.get('/users/info')
+			.set('Authorization', `Bearer ${login.body.jwt}`);
+		expect(res.body.email).toBe('a@a.ru');
+	});
+
+	it('Info - error', async () => {
+		const res = await request(application.app).get('/users/info').set('Authorization', `Bearer 1`);
+		expect(res.statusCode).toBe(401);
+	});
 });
 
 afterAll(() => {
